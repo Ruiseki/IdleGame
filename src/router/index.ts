197f5e89a6a0691407d094e506f550aab74c5d65
Ruiseki <jojobru60@gmail.com>
2023-10-03 21:25:42 +0200

    primal structur

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,13 +10,15 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/inventory',
+      name: 'inventory',
+      component: () => import('../views/InventoryView.vue')
+    },
+    {
+      path: '/shop',
+      name: 'shop',
+      component: () => import('../views/ShopView.vue')
+    },
   ]
 })
 
