f504bbd5270a39b9d3057ae952f3ac146425000b
Ruiseki <jojobru60@gmail.com>
2023-10-04 17:01:27 +0200

    localStorage

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,35 +1,63 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+
 export const useCounterStore = defineStore('counter', () => {
 
-  const quantity = ref(0)
-  const mainCoef = ref(1)
-  const clickCoef = ref(1)
+  let backupQty: any = localStorage.getItem('quantity');
+  if (backupQty == null) backupQty = '0';
+  backupQty = Number.parseInt(backupQty);
+
+  let backupCoef: any = localStorage.getItem('coefQty');
+  if (backupCoef == null) backupCoef = '1';
+  backupCoef = Number.parseInt(backupCoef);
+
+  let backupCoefClick: any = localStorage.getItem('coefClick');
+  if (backupCoefClick == null) backupCoefClick = '1';
+  backupCoefClick = Number.parseInt(backupCoefClick);
+
+  const quantity = ref(backupQty)
+  const mainCoef = ref(backupCoef)
+  const clickCoef = ref(backupCoefClick)
 
   setInterval(() => {
     quantity.value += 1 * mainCoef.value
+    localStorage.setItem('quantity', quantity.value.toString())
+    localStorage.setItem('coefQty', mainCoef.value.toString())
+    localStorage.setItem('coefClick', clickCoef.value.toString())
+    
   }, 5000)
 
   function mainClick()
   {
       quantity.value += 1 * clickCoef.value
-      console.log(quantity);
+      localStorage.setItem('quantity', quantity.value.toString())
+      localStorage.setItem('coefQty', mainCoef.value.toString())
+      localStorage.setItem('coefClick', clickCoef.value.toString())
   }
 
   return { quantity, mainCoef, clickCoef, mainClick }
 })
 
 export const useCounterMoney = defineStore('counterMoney', () => {
-  
-    const money = ref(0)
-    const mainCoef = ref(1)
+
+    let backupMoney: any = localStorage.getItem('money');
+    if (backupMoney == null) backupMoney = '0';
+    backupMoney = Number.parseInt(backupMoney);
+
+    let backupMoneyCoef: any = localStorage.getItem('moneyCoef');
+    if (backupMoneyCoef == null) backupMoneyCoef = '1';
+    backupMoneyCoef = Number.parseInt(backupMoneyCoef);
+
+    const money = ref(backupMoney)
+    const moneyCoef = ref(backupMoneyCoef)
     
     function mainClick( students: number)
     {
-        money.value += students * mainCoef.value
-        console.log(money);
+        money.value += students * moneyCoef.value
+        localStorage.setItem('money', money.value.toString())
+        localStorage.setItem('moneyCoef', moneyCoef.value.toString())
     }
   
-    return { money, mainCoef, mainClick }
-  })
+    return { money, moneyCoef, mainClick }
+})
